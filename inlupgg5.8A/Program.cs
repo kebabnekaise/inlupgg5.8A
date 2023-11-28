d3e52511561766aed34de773467db1803a896603
kebabnekaise <emsi06001@utb.vaxjo.se>
2023-11-28 14:30:31 +0100

    Add project files.

---
+++ inlupgg5.8A/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace inluppg5_7A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nummer = { 1111, 3333, 45, 9999999, 77777};
+            string[] fakeNummer = new string[nummer.Length];
+
+            for (int i = 0; i < nummer.Length; i++)
+            {
+                fakeNummer[i] = nummer[i].ToString();
+            }
+
+            System.Threading.Thread.Sleep(300);
+
+            for(int i = 0;  i < nummer.Length; i++)
+            {
+                for (int j = 0; j < fakeNummer[i].Length; j++)
+                {
+
+                    Console.Write(fakeNummer[i][0]);
+                    System.Threading.Thread.Sleep(300);
+                }
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(600);
+            }
+        }
+    }
+}
